27fc52eefc42b81c7b2daf22a7015ce5362883b6
ricardo <ricardo.br.pi@gmail.com>
2022-01-19 21:35:49 -0300

    feat: create write pattern validation from a cpf

--- pages/passwordchecker.js
+++ pages/passwordchecker.js
@@ -2,20 +2,39 @@ import React, { useState } from "react";
 
 function PasswordChecker() {
   const [password, setPassword] = useState("");
-  const regex = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'\s]+$/;
+  const [cpf, setCpf ] = useState("");
+  const regexAbc = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'\s]+$/;
+  const regexCpf = /[0-9]{3}[.][0-9]{3}[.][0-9]{3}[-][0-9]{2}/;
 
-  function checker() {
-    if (password.length <= 8 && regex.test(password)) {
+  function checkerPassword() {
+    if (password.length <= 8 && regexAbc.test(password)) {
       alert("Senha não tem 8 caracteres")
+    }
+  }
+
+  function checkerCpf() {
+    if (regexCpf.test(cpf)) {
+      alert("Tudo certo")
+    }
+    else {
+      alert("CPF invalido")
     }
   }
 
   return (
     <div>
-      <p>Senha {password}</p>
-      <input type="text" onChange={(event) => setPassword(event.target.value)} />
-      <button onClick={checker}>to check</button>
-      <button onClick={() => setPassword("")}>Clear</button>
+      <div>
+        <p>Senha {password}</p>
+        <input type="text" onChange={(event) => setPassword(event.target.value)}/>
+        <button onClick={checkerPassword}>to check</button>
+        <button onClick={() => setPassword("")}>Clear</button>
+      </div>
+      <div>
+        <p>Digite o seu CPF</p>
+        <input type="text" onChange={(event) => setCpf(event.target.value)}/>
+        <button onClick={checkerCpf}>to check</button>
+        <button onClick={() => setCpf("")}>Clear</button>
+      </div>
     </div>
   )
 }
