fa29002d72967277dcb4f32598dd491051db7617
ricardo <ricardo.br.pi@gmail.com>
2022-01-21 22:01:36 -0300

    feat: add function to insert your name and clear values

--- pages/passwordchecker.js
+++ pages/passwordchecker.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function PasswordChecker() {
   const [password, setPassword] = useState("");
   const [cpf, setCpf ] = useState("");
   const regexAbc = /^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ'\s]+$/;
   const regexCpf = /[0-9]{3}[.][0-9]{3}[.][0-9]{3}[-][0-9]{2}/;
+  const [name, setName] = useState("")
 
   function checkerPassword() {
-    if (password.length <= 8 && regexAbc.test(password)) {
+    if (password.length <= 7 && regexAbc.test(password)) {
       alert("Senha não tem 8 caracteres")
     }
   }
@@ -17,8 +18,13 @@ function PasswordChecker() {
       alert("Tudo certo")
     }
     else {
-      alert("CPF invalido")
+      alert("Siga o exemplo")
     }
+  }
+
+  function clearInput(clear) {
+    clear("")
+    console.log("fsdf")
   }
 
   return (
@@ -27,13 +33,19 @@ function PasswordChecker() {
         <p>Senha {password}</p>
         <input type="text" onChange={(event) => setPassword(event.target.value)}/>
         <button onClick={checkerPassword}>to check</button>
-        <button onClick={() => setPassword("")}>Clear</button>
+        <button onClick={() => clearInput(setPassword)}>Clear</button>
       </div>
       <div>
         <p>Digite o seu CPF</p>
+        <p>Ex: 000.000.000-00</p>
         <input type="text" onChange={(event) => setCpf(event.target.value)}/>
         <button onClick={checkerCpf}>to check</button>
-        <button onClick={() => setCpf("")}>Clear</button>
+        <button onClick={() => clearInput(setCpf)}>Clear</button>
+      </div>
+      <div>
+        <p>Seu nome: {name}</p>
+        <input type="text" onChange={(e) => setName(e.target.value)}/>
+        <button onClick={() => clearInput(setName)}>Clear</button>
       </div>
     </div>
   )
